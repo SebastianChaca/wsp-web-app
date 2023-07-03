d08faa2acf153ae091682676d0bf9991bac9bc02
Seba Chaca <sebastian@sebastians-MacBook-Pro.local>
2023-07-03 18:51:16 -0300

    notifications

--- backend/constrollers/notifications.js
+++ backend/constrollers/notifications.js
@@ -1,13 +1,22 @@
 const ChatUser = require('../models/usuario');
 //update de las notificaciones que le mando a un amigo cuando le escribo
-const updateNotificationsMessage = async (myId, friendId, notifCount) => {
-  //TODO traer la cantidad de notif que tiene el user para sumarle una
-  await ChatUser.findOneAndUpdate(
-    { _id: friendId },
-    //TODO: esta logica tiene que sumar de a uno
-    { friends: { user: myId, $inc: { notifications: notifCount + 1 } } },
-    { new: true }
-  );
+const updateNotificationsMessage = async (userId, friendUserId) => {
+  try {
+    const updatedUser = await ChatUser.findOneAndUpdate(
+      {
+        _id: friendUserId,
+        'friends.user': userId,
+      },
+      { $inc: { 'friends.$.notifications': 1 } },
+      { new: true }
+    );
+
+    return updatedUser;
+  } catch (error) {
+    console.error(error);
+    // Handle the error
+    return null;
+  }
 };
 
 const test = async (req, res) => {
@@ -37,15 +46,22 @@ const resetNotificationsMessage = async (req, res) => {
   const myId = req.uid;
   const friendId = req.body.uid;
 
-  await ChatUser.findOneAndUpdate(
-    { _id: myId, 'friends.user': friendId }, // Match the user and the specific friend
+  try {
+    const updatedUser = await ChatUser.findOneAndUpdate(
+      {
+        _id: myId,
+        'friends.user': friendId,
+      },
+      { $set: { 'friends.$.notifications': 0 } },
+      { new: true }
+    );
 
-    { $set: { 'friends.$.notifications': 0 } }, // Update the notifications field of the matched friend
-    { new: true }
-  );
-  return res.json({
-    ok: true,
-  });
+    return updatedUser;
+  } catch (error) {
+    console.error(error);
+    // Handle the error
+    return null;
+  }
 };
 
 module.exports = {
--- backend/models/sockets.js
+++ backend/models/sockets.js
@@ -79,9 +79,7 @@ class Sockets {
 
         const message = await saveMessage(payload);
 
-        //TODO: solo setea en 1
-        //await updateNotificationsMessage(uid, payload.to);
-
+        await updateNotificationsMessage(uid, payload.to);
         //emito mensaje al destinatario
         this.io.to(payload.to).emit('personal-message', message);
         //TODO: puedo hacer esto en la UI para evitar otra request
