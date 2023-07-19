6fe91e5f2f457420c3cb98adcaa20add7e0e6bad
Seba Chaca <sebastian@sebastians-MacBook-Pro.local>
2023-07-18 22:47:37 -0300

    close btn

--- frontend/src/components/Ui/ResponseToMessageUI/CloseButton/CloseButton.tsx
+++ frontend/src/components/Ui/ResponseToMessageUI/CloseButton/CloseButton.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
-import { GrClose } from 'react-icons/all';
 import { useDispatch } from 'react-redux';
-import { IconButton } from '@chakra-ui/react';
+import { CloseButton as Close } from '@chakra-ui/react';
 import { setResponseTo } from '../../../../redux/activeChat/activeChatSlice';
 
 const CloseButton: FC = () => {
@@ -10,15 +9,7 @@ const CloseButton: FC = () => {
   const handleClose = () => {
     dispatch(setResponseTo(null));
   };
-  return (
-    <IconButton
-      onClick={handleClose}
-      aria-label=""
-      icon={<GrClose size="20px" />}
-      bg="transparent"
-      _hover={{ bg: 'brand.backgroundWhite' }}
-    />
-  );
+  return <Close onClick={handleClose} fontSize="15px" />;
 };
 
 export default CloseButton;
